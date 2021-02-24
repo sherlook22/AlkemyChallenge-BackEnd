93602f6a22972e90b2f0189a816e84787dd3ee3c
Sherlook <re.gonzalo@yahoo.com.ar>
2021-02-24 18:42:53 -0300

    Created the user controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { User } from "../models/user.model";
+
+class UserController {
+    public async index(req: Request, res: Response) {
+        //
+    }
+}
+
+export default new UserController;
