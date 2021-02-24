f33d61642017a68b60e2b10cf39b9e15c1a394f6
Sherlook <re.gonzalo@yahoo.com.ar>
2021-02-24 19:21:38 -0300

    Added the hash for password before create user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,9 +1,13 @@
-import { Table } from "sequelize-typescript";
+import { BeforeCreate, Table } from 'sequelize-typescript';
 import { UserDefinition } from './definitions/userDefinition'
+import bcrypt from 'bcrypt';
 
 @Table
 export class User extends UserDefinition {
   
-    //
+    @BeforeCreate
+    static hashPassword(user: User) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
+    }
 
 }
