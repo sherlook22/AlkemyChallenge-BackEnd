b199440a54250e9b567cca25285bcf0a93c9532a
Sherlook <re.gonzalo@yahoo.com.ar>
2021-02-27 02:19:16 -0300

    Before update validate amount

--- src/models/operation.model.ts
+++ src/models/operation.model.ts
@@ -1,4 +1,4 @@
-import { BeforeCreate, BelongsTo, Column, ForeignKey, Table } from "sequelize-typescript";
+import { BeforeBulkUpdate, BeforeCreate, BeforeUpdate, BelongsTo, Column, ForeignKey, Table } from "sequelize-typescript";
 import { OperationDefinition } from './definitions/operationDefiniton';
 import { Type } from './type.model';
 import { User } from './user.model'
@@ -18,21 +18,17 @@ export class Operation extends OperationDefinition {
   @BelongsTo(() => User)
   user!: User
 
-
+  @BeforeUpdate
   @BeforeCreate
   static async validateAmount(op: Operation) {
+
+    console.log(op);
+    
     const { typeId } = op;
     const opType = await Type.findOne({where: { id: typeId }});
-
-    console.log(typeId);
     
-
-    if(opType.name === 'Ingreso') {
-      op.amount = op.amount
-    }
-
     if(opType.name === 'Egreso') {
-      op.amount = -op.amount;
+      op.amount = op.amount >= 0 ? -op.amount : op.amount;
     }
   }
 
